8bc572465921424aeb6702222fa43ed6284a79ce
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-08-29 19:05:19 +0530

    assignments

---
+++ add.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("enter any number\n");
+    scanf("%d %d", &num1, &num2);
+    printf("your addition of number is %d",num1+num2);
+    return 0;
+}
