57d7912d52faa03bdad43ef2e380ff7f199b0973
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-08 22:13:28 +0530

    Add files via upload

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Select any two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("multiplication of numbers is %d\n", num1 * num2);
+    return 0;
+}
