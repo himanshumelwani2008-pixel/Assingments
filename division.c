f15533cb290632bfec89e86cf073007c684539b6
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-08 22:14:38 +0530

    Add files via upload

---
+++ division.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+
+    printf("Select any two numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("division of numbers is %d\n", num1 / num2);
+    return 0;
+}
