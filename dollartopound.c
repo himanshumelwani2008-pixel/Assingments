d4362641e5dc2a2ae5cf43d8ba6beec1c8c0f291
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-09 01:33:08 +0530

    Add files via upload

---
+++ dollartopound.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1;
+    printf("choose how many dollar you have to convert in pound\n");
+    scanf("%f", &num1);
+
+    printf( "%f rupees is equal to %f dollars ", num1,num1*1.453);
+    return 0;
+}
