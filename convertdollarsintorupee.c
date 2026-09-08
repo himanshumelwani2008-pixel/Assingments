c2db7770c2023d9643b103ee6bd7643db603cb9d
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-08 23:42:43 +0530

    Add files via upload

---
+++ convertdollarsintorupee.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1;
+    printf("choose how many dollars you have to convert in rupees\n");
+    scanf("%d", &num1);
+
+    printf( "%d dollars is equal to %d rupees ", num1,num1*48);
+    return 0;
+}
