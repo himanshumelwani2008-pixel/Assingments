79ebaf968b2bffdd63f2094bfebea8ebc9722a29
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-13 00:15:31 +0530

    Add files via upload

---
+++ triangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("what is the height of triangle? \n");
+    scanf("%d",&num1);
+    printf("whtat is base of triangle?");
+    scanf("%d",&num2);
+    printf("area of triangle = %d\n",num1*num2/2);
+    
+    return 0;
+
+}
