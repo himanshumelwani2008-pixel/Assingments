bc1eed9e21372e32e3c104bfc785537ed19e5933
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-13 00:01:48 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num1;
+    printf("what is the length  of side of square\n");
+    scanf("%d",&num1);
+    printf("area of square = %d\n",num1*num1);
+    printf("perimeter of square= %d",4*num1);
+    return 0;
+
+}
