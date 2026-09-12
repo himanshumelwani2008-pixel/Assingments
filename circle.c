51de68c261727c3c45ac0c182839b761f16fb8c3
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-13 00:10:57 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num1;
+    printf("what is the radius of circle\n");
+    scanf("%d",&num1);
+    printf("area of circle = %d\n",num1*num1*22/7);
+    
+    return 0;
+
+}
