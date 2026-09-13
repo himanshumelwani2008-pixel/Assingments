dfb4287bb98cc5b19e08131608bf15f75118b13f
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-14 01:45:52 +0530

    Add files via upload

---
+++ tillNodd.no.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("till how much you have to print\n");
+    scanf("%d",&num2);
+    for(num1=1;num1<=num2;num1+=2)
+    {
+        printf("%d\n",num1);
+    }
+    return 0;
+}
