b50d074d1a84e2acd01b0b9af8f75aa35da16cdc
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-14 02:16:10 +0530

    Add files via upload

---
+++ nameNtimes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2;
+    char name[30];
+    printf("what is your name?\n");
+    scanf("%29s",&name);
+    printf("how much times you have to print it?\n");
+    scanf("%d",&num2);
+    for(num1=1;num1<=num2;num1++)
+    {
+        printf("%d %s\n",num1,name);
+    }
+    return 0;
+    
+}
