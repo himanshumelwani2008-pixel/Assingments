29b305d2ac9c42bfc3157b82aab2b93f9db76663
himanshumelwani2008-pixel <himanshumelwani2008@gmail.com>
2026-09-15 14:59:30 +0530

    Add files via upload

---
+++ seperatedigits.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1,digits;
+    printf("choose any number\n");
+    scanf("%d",&num1);
+    while (num1>0)
+    {
+        digits = num1%10;
+        printf("%d",digits);
+        num1=num1/10;
+    }
+    return 0;
+}
